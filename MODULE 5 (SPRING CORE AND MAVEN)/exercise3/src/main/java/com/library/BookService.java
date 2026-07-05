4eeec8b4ab2645fe3c3d3d7eebea2c6010d3d2d1
Shanmathi K <shanmathi06.k@gmail.com>
2026-07-06 00:50:21 +0530

    Add files via upload

---
+++ MODULE 5 (SPRING CORE AND MAVEN)/exercise3/src/main/java/com/library/BookService.java
@@ -0,0 +1,23 @@
+package com.library;
+
+import java.util.List;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<String> getAllBooks() {
+        return bookRepository.findAllBooks();
+    }
+
+    public void printAllBooks() {
+        System.out.println("Books in library:");
+        for (String book : getAllBooks()) {
+            System.out.println(" - " + book);
+        }
+    }
+}
